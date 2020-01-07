537487ad7b04fd0d5c58b86a70cb97651006616e
yash-bhandari <yashaswi@ualberta.ca>
2020-01-07 08:55:31 -0700

    adedd modify user library to auth scope

--- src/Components/LoginButton.js
+++ src/Components/LoginButton.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import {Button} from '@material-ui/core';
 
-
+const scope = 'user-library-read user-library-modify';
 const authLink =
-    "https://accounts.spotify.com/authorize?response_type=token&client_id=dfaef265d1624a78810ee848076a1eb6&scope=user-library-read";
+    "https://accounts.spotify.com/authorize?response_type=token&client_id=dfaef265d1624a78810ee848076a1eb6&scope="+encodeURIComponent(scope);
 
-export const LoginButton = ({ authorized, setAccessToken, loginButton }) => {
+export const LoginButton = ({ authorized, setAccessToken }) => {
     useEffect(() => handleRedirect(authorized, setAccessToken), []);
 
     return (
