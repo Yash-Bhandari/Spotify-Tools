b60d6bd2e9e609508cca326480ef4d136555aef7
yash-bhandari <yashaswi@ualberta.ca>
2020-01-07 08:54:26 -0700

    added removeTracks to ServerLiason

--- src/utils/ServerLiason.js
+++ src/utils/ServerLiason.js
@@ -23,7 +23,7 @@ export function ServerLiason(authKey) {
             .then(json => json.items)
 
 
-        while (items.length > 0 && tracks.length < 500) {
+        while (items.length > 0 && tracks.length < 1500) {
             tracks = tracks.concat(items.map(
                 item => item.track
             ));
@@ -34,6 +34,26 @@ export function ServerLiason(authKey) {
                 .then(json => json.items)
         }
         console.log(tracks)
+        return true;
+    }
+
+    /**
+     * Queries the spotify api to remove the specified tracks from the user library.
+     * @param tracks An array of tracks to remove.
+     */
+    this.removeTracks = async (tracks) => {
+        const url = this.url + '/tracks';
+        const ids = tracks.map(track => track.id);
+
+        // maximum of 50 songs can be removed at a time
+        for (let i = 0; i < ids.length; i+=50) {
+            await fetch(url, {
+                method: 'DELETE',
+                headers: this.headers,
+                body: JSON.stringify(ids.slice(i, i+50))
+            })
+        }
+
         return true;
     }
 }
