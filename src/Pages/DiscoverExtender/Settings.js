163f3730e184e9e1db7e555e693e3f64c3c5d4cc
yash-bhandari <yashaswi@ualberta.ca>
2020-01-10 17:03:51 -0700

    enhanced extender wizard formatting

--- src/Pages/DiscoverExtender/Settings.js
+++ src/Pages/DiscoverExtender/Settings.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { Grid, Checkbox, TextField, FormControlLabel, Typography, makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Grid, Checkbox, TextField, FormControlLabel, FormControl, Typography, makeStyles, InputLabel, Select, MenuItem } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
+    root: {
+        'margin': theme.spacing(2),
+    },
     text: {
         'padding-left': theme.spacing(4),
-        'padding-right': theme.spacing(4)
+        'padding-right': theme.spacing(4),
+        'margin-top': 0
+    },
+    input: {
+        'margin-top': theme.spacing(1)
     }
 }))
 
 export const Settings = ({ settings, setSettings }) => {
-
+    //const [selectPlaylist, setSelectPlaylist] = useState(false);
     const handleChange = name => val => {
         if (name === 'size')
 
@@ -23,53 +30,59 @@ export const Settings = ({ settings, setSettings }) => {
     const classes = useStyles();
 
     return (
-        <Grid item container justify='space-around' spacing={2}>
+        <Grid item container justify='space-around' spacing={2} className={classes.root}>
             <Grid item lg={12}>
                 <Typography align='center' className={classes.text}>
                     Choose the number and type of songs to include in your playlist.
                     You can also choose the playlist's name (we won't overwrite any existing playlists with the same name).
                 </Typography>
             </Grid>
-            <Grid item>
-                <TextField
-                    InputLabelProps={{ shrink: true }}
-                    type='number'
-                    defaultValue={0}
-                    label='Playlist Size'
-                    value={settings.size}
-                    onChange={e => handleChange('size')(e.target.value)}
-                />
-            </Grid>
-            <Grid item>
-                <TextField
-                    InputLabelProps={{ shrink: true }}
-                    label={'Playlist Name'}
-                    value={settings.playlistName}
-                    onChange={e => handleChange('playlistName')(e.target.value)}
-                />
-            </Grid>
-            <Grid item>
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={settings.explicit}
-                            onChange={e => handleChange('explicit')(e.target.checked)}
-                        />
-                    }
-                    label='Allow Explicit Songs'
-                />
-            </Grid>
-            <Grid item>
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={settings.library}
-                            onChange={e => handleChange('library')(e.target.checked)}
-                        />
-                    }
-                    label='Allow Songs Already In Library'
-                />
-            </Grid>
+            <TextField
+                className={classes.input}
+                InputLabelProps={{ shrink: true }}
+                type='number'
+                label='Playlist Size'
+                value={settings.size}
+                onChange={e => handleChange('size')(e.target.value)}
+            />
+            <TextField
+                className={classes.input}
+                InputLabelProps={{ shrink: true }}
+                label={'Playlist Name'}
+                value={settings.playlistName}
+                onChange={e => handleChange('playlistName')(e.target.value)}
+            />
+            <FormControlLabel
+                className={classes.input}
+                control={
+                    <Checkbox
+                        checked={settings.explicit}
+                        onChange={e => handleChange('explicit')(e.target.checked)}
+                    />
+                }
+                label='Allow Explicit Songs'
+            />
+            <FormControlLabel
+                className={classes.input}
+                control={
+                    <Checkbox
+                        checked={settings.library}
+                        onChange={e => handleChange('library')(e.target.checked)}
+                    />
+                }
+                label='Allow Songs Already In Library'
+            />
+
+            {/*<FormControl className={classes.input} style={{minWidth: 240}}>
+                <InputLabel id='seed-playlist'>Seed Playlist</InputLabel>
+                <Select
+                    value={settings.seedPlaylist}
+                    onChange={e => handleChange('seedPlaylist')(e.target.value)}
+                >
+                    <MenuItem value={0}>Discover Weekly</MenuItem>
+                </Select>
+            </FormControl>*/}
+
         </Grid>
     )
 }
