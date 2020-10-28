0e7791ecf305cf077ae2eed71dadb2c6d11b77de
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2020-10-28 14:15:31 -0600

    changed header font to match logo

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -8,14 +8,16 @@ import {
 } from "@material-ui/core";
 import MenuDrawer from "./MenuDrawer";
 import LoginButton from "../LoginButton";
-import logo from "./spotify_logo.png";
+import logo from "./spotify_logo (Small).png";
 
 const useStyles = makeStyles({
   title: {
     flexGrow: 1,
+    fontFamily: "Montserrat",
+    fontWeight: 600,
   },
   spotifyLogo: {
-    width: "180px",
+    width: "170px",
   },
 });
 
@@ -26,7 +28,7 @@ export default (props) => {
       <Toolbar>
         <MenuDrawer />
         <img className={classes.spotifyLogo} src={logo} alt="Logo" />
-        <Typography className={classes.title} variant="h4">
+        <Typography className={classes.title} variant="h4" color="secondary">
           {" Tools"}
         </Typography>
         {props.children}
